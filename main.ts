4924bac3f3deb9ed0312100031a3a69b9a5a9034
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-06-08 16:03:57 -0700

    Allow reverting changes

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface SmartTypographySettings {
   curlyQuotes: boolean;
@@ -12,79 +12,132 @@ const DEFAULT_SETTINGS: SmartTypographySettings = {
   ellipsis: true,
 };
 
-type InputRule = [
-  string,
-  RegExp,
-  (delta: CodeMirror.EditorChangeCancellable) => void
-];
+interface InputRule {
+  matchTrigger: string;
+  matchRegExp: RegExp;
+  performUpdate: (
+    instance: CodeMirror.Editor,
+    delta: CodeMirror.EditorChangeCancellable
+  ) => void;
+  performRevert: (
+    instance: CodeMirror.Editor,
+    delta: CodeMirror.EditorChangeCancellable
+  ) => void;
+}
 
-const emDash: InputRule = [
-  "-",
-  /--$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const emDash: InputRule = {
+  matchTrigger: "-",
+  matchRegExp: /--$/,
+  performUpdate: (instance, delta) => {
     delta.update({ line: delta.from.line, ch: delta.from.ch - 1 }, delta.to, [
       "—",
     ]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "—") {
+      delta.update(delta.from, delta.to, ["--"]);
+    }
+  },
+};
 
-const ellipsis: InputRule = [
-  ".",
-  /\.\.\.$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const ellipsis: InputRule = {
+  matchTrigger: ".",
+  matchRegExp: /\.\.\.$/,
+  performUpdate: (instance, delta) => {
     delta.update({ line: delta.from.line, ch: delta.from.ch - 2 }, delta.to, [
       "…",
     ]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "…") {
+      delta.update(delta.from, delta.to, ["..."]);
+    }
+  },
+};
 
-const openDoubleQuote: InputRule = [
-  '"',
-  /(?:^|[\s\{\[\(\<'"\u2018\u201C])(")$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const openDoubleQuote: InputRule = {
+  matchTrigger: '"',
+  matchRegExp: /(?:^|[\s\{\[\(\<'"\u2018\u201C])(")$/,
+  performUpdate: (instance, delta) => {
     delta.update(delta.from, delta.to, ["“"]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "“") {
+      delta.update(delta.from, delta.to, ['"']);
+    }
+  },
+};
 
-const closeDoubleQuote: InputRule = [
-  '"',
-  /"$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const closeDoubleQuote: InputRule = {
+  matchTrigger: '"',
+  matchRegExp: /"$/,
+  performUpdate: (instance, delta) => {
     delta.update(delta.from, delta.to, ["”"]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "”") {
+      delta.update(delta.from, delta.to, ['"']);
+    }
+  },
+};
 
-const pairedDoubleQuote: InputRule = [
-  '""',
-  /""$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const pairedDoubleQuote: InputRule = {
+  matchTrigger: '""',
+  matchRegExp: /""$/,
+  performUpdate: (instance, delta) => {
     delta.update(delta.from, delta.to, ["“”"]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "“") {
+      delta.update(delta.from, { ...delta.to, ch: delta.to.ch + 1 }, ['""']);
+      setTimeout(() =>
+        instance.setCursor({ ...delta.from, ch: delta.from.ch + 1 })
+      );
+    }
+  },
+};
 
-const openSingleQuote: InputRule = [
-  "'",
-  /(?:^|[\s\{\[\(\<'"\u2018\u201C])(')$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const openSingleQuote: InputRule = {
+  matchTrigger: "'",
+  matchRegExp: /(?:^|[\s\{\[\(\<'"\u2018\u201C])(')$/,
+  performUpdate: (instance, delta) => {
     delta.update(delta.from, delta.to, ["‘"]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "‘") {
+      delta.update(delta.from, delta.to, ["'"]);
+    }
+  },
+};
 
-const closeSingleQuote: InputRule = [
-  "'",
-  /'$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const closeSingleQuote: InputRule = {
+  matchTrigger: "'",
+  matchRegExp: /'$/,
+  performUpdate: (instance, delta) => {
     delta.update(delta.from, delta.to, ["’"]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "’") {
+      delta.update(delta.from, delta.to, ["'"]);
+    }
+  },
+};
 
-const pairedSingleQuote: InputRule = [
-  "''",
-  /''$/,
-  (delta: CodeMirror.EditorChangeCancellable) => {
+const pairedSingleQuote: InputRule = {
+  matchTrigger: "''",
+  matchRegExp: /''$/,
+  performUpdate: (instance, delta) => {
     delta.update(delta.from, delta.to, ["‘’"]);
   },
-];
+  performRevert: (instance, delta) => {
+    if (instance.getRange(delta.from, delta.to) === "‘") {
+      delta.update(delta.from, { ...delta.to, ch: delta.to.ch + 1 }, ["''"]);
+      setTimeout(() =>
+        instance.setCursor({ ...delta.from, ch: delta.from.ch + 1 })
+      );
+    }
+  },
+};
 
 const emDashRules = [emDash];
 const ellipsisRules = [ellipsis];
@@ -97,40 +150,128 @@ const smartQuoteRules = [
   pairedSingleQuote,
 ];
 
+type ChangeHandler = (
+  instance: CodeMirror.Editor,
+  delta: CodeMirror.EditorChangeCancellable
+) => void;
+
 export default class SmartTypography extends Plugin {
   settings: SmartTypographySettings;
   inputRules: InputRule[] = [];
+
+  didJustUpdate: { [id: string]: InputRule | undefined } = {};
+  changeHandlers: { [id: string]: ChangeHandler } = {};
 
   async onload() {
     await this.loadSettings();
 
     this.addSettingTab(new SmartTypographySettingTab(this.app, this));
 
-    this.registerCodeMirror((cm) => {
-      cm.on("beforeChange", (instance, delta) => {
-        if (delta.origin === "+input" && delta.text.length === 1) {
-          const input = delta.text[0];
-          const rules = this.inputRules.filter((r) => r[0] === input);
+    this.app.workspace.on("layout-change", this.handleLayoutChange);
+    this.app.workspace.onLayoutReady(() => {
+      this.registerCodeMirror((cm: CodeMirror.Editor) => {
+        this.getCodeMirrorID(cm, (id) => {
+          if (!id) return;
 
-          if (rules.length === 0) return;
+          const handleBeforeChange = this.getBeforeChangeHandler(id);
+          this.changeHandlers[id] = handleBeforeChange;
+          cm.on("beforeChange", handleBeforeChange);
+        });
+      });
+    });
+  }
 
-          let str = input;
+  onunload() {
+    this.app.workspace.off("layout-change", this.handleLayoutChange);
+    this.app.workspace.iterateCodeMirrors((cm) => {
+      this.getCodeMirrorID(cm, (id) => {
+        if (!id) return;
+        cm.off("beforeChange", this.changeHandlers[id]);
+      });
+    });
+  }
 
-          if (delta.to.ch > 0) {
-            str = `${instance.getRange(
-              { line: delta.to.line, ch: 0 },
-              delta.to
-            )}${str}`;
-          }
+  getBeforeChangeHandler(id: string) {
+    return (
+      instance: CodeMirror.Editor,
+      delta: CodeMirror.EditorChangeCancellable
+    ) => {
+      if (this.didJustUpdate[id] && delta.origin === "+delete") {
+        this.didJustUpdate[id].performRevert(instance, delta);
+        this.didJustUpdate[id] = undefined;
+        return;
+      }
 
-          for (let rule of rules) {
-            if ((rule[1] as RegExp).test(str)) {
-              rule[2](delta);
-              break;
-            }
+      if (delta.origin === "+input" && delta.text.length === 1) {
+        const input = delta.text[0];
+        const rules = this.inputRules.filter((r) => r.matchTrigger === input);
+
+        if (rules.length === 0) {
+          this.didJustUpdate[id] = undefined;
+          return;
+        };
+
+        let str = input;
+
+        if (delta.to.ch > 0) {
+          str = `${instance.getRange(
+            { line: delta.to.line, ch: 0 },
+            delta.to
+          )}${str}`;
+        }
+
+        for (let rule of rules) {
+          if (rule.matchRegExp.test(str)) {
+            this.didJustUpdate[id] = rule;
+            rule.performUpdate(instance, delta);
+            return;
           }
         }
-      });
+      }
+
+      if (this.didJustUpdate[id]) {
+        this.didJustUpdate[id] = undefined;
+      }
+    };
+  }
+
+  handleLayoutChange = () => {
+    const leafIds: { [id: string]: boolean } = {};
+    const didJustUpdate: { [id: string]: InputRule | undefined } = {};
+    const changeHandlers: { [id: string]: ChangeHandler } = {};
+
+    this.app.workspace.iterateAllLeaves((leaf) => {
+      if (leaf.view instanceof MarkdownView) {
+        const id = (leaf as any).id;
+        leafIds[id] = true;
+      }
+    });
+
+    Object.keys(this.didJustUpdate).forEach((k) => {
+      if (leafIds[k]) {
+        didJustUpdate[k] = this.didJustUpdate[k];
+        changeHandlers[k] = this.changeHandlers[k];
+      }
+    });
+
+    this.didJustUpdate = didJustUpdate;
+    this.changeHandlers = changeHandlers;
+  };
+
+  getCodeMirrorID(cm: CodeMirror.Editor, cb: (id: string | null) => void) {
+    // Wait for the layout to settle
+    setTimeout(() => {
+      const gutter = cm.getGutterElement();
+      const markdownLeaves = this.app.workspace.getLeavesOfType("markdown");
+      const containingLeaf = markdownLeaves.find((l) =>
+        l.view.containerEl.contains(gutter)
+      );
+
+      if (containingLeaf) {
+        return cb((containingLeaf as any).id);
+      }
+
+      cb(null);
     });
   }
 
